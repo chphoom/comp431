b002d5a9d337a325e56b04e72637a8cb7ce3126b
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2022-08-29 23:03:50 -0400

    Update helper.py

--- hw1/helper.py
+++ hw1/helper.py
@@ -8,6 +8,7 @@ def validPath(string):
     sList = re.split('@', string)
     if len(sList)!=2: return False
     local = sList[0]
-    if special.values() in local: return False
+    for s in special:
+        if s in local: return False
     domain = sList[1]
     return string[1] != "<" or string[-1]!=">" or "@" in string
