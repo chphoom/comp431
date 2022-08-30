7e9ec66705f48ee45ab5a943cc675abbeb630987
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2022-08-29 21:55:47 -0400

    first try

---
+++ hw1/SMTPServer.py
@@ -0,0 +1,66 @@
+import fileinput
+import re
+import helper
+
+
+status = {
+    "MAIL": 1,
+    "RCPT": 2,
+    "DATA": 3,
+    "250 OK": 250,
+    "354 Start mail input; end with . on a line by itself": 354,
+    ".": 4,
+    "500 Syntax error: command unrecognized": 500,
+    "501 Syntax error in parameters or arguments": 501,
+    "503 Bad sequence of commands": 503,
+}#end of dictionary
+
+output = []
+
+# Your program should be structured as a loop that:
+# • Reads a line of input from standard input (the keyboard in Linux).
+currStatus = status[1];
+for line in fileinput.input():
+    if currStatus == status[354]:
+        output.append(line)
+    # • If/when “end-of-file” is reached on standard input (i.e., when control-D is typed from the keyboard
+    # under Linux), terminate your program. Otherwise...
+    # • Echo the line of input to standard output (i.e., print the line of input exactly as it was input to
+    if line == "\n" & currStatus == status[354]:
+        output.append(status[4])
+        print(status[4])
+        print(status[250])
+        currStatus == status[1]
+        f.write(output)
+    else: print(line)
+    # standard output [i.e., to the Linux window in which you entered the command to execute your
+    # program]).
+    #print("\n*" + line, end='')
+    # • When well-formed (syntax and order) SMTP commands are read, print responses as described
+    # below in section titled “Responses for Well-Formed Commands”.
+    lineList = re.split('\s\t\0\n', line)
+    command = lineList[0];
+    # • For invalid (ill-formed) commands, print out the error message as described in the section below
+    # titled “Error Processing”.
+    if command in status.values():
+        if command == status[1] == currStatus:
+            if lineList[2]!="FROM:" | helper.validPath(lineList[3])==False: #: add check valid path
+                print(status[501])
+            else: 
+                output.append(line)
+                currStatus = status[2]
+        elif command == status[2] == currStatus:
+            if lineList[2]!="TO:" | helper.validPath(lineList[3])==False: #: add check for valid path
+                print(status[501])
+            else: 
+                path = "forward/" + lineList[3]
+                f = open(path,"w+")
+                currStatus = status[3]
+                output.append(line)
+        elif command == status[3] == currStatus:
+            currStatus = status[354]
+            output.append(line)
+        else:
+            print(status[503])
+    elif currStatus != status[354]:
+        print(status[500])
---
+++ hw1/helper.py
@@ -0,0 +1,14 @@
+import re
+
+special = {
+    "<",">","(",")","[","]","\\","."
+| ",",";",":","@","\""
+}
+
+def validPath(string):
+    sList = re.split('@', string)
+    if len(sList!=2): return False
+    local = sList[0]
+    if special.values() in local: return False
+    domain = sList[1]
+    return string[1] != "<"|string[-1]!=">"|"@" in string
